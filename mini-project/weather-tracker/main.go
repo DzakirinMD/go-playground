c215051e93311be1db9a9dae8e01275b65a7fc3c
DzakirinMD <dzakirinmd@gmail.com>
2022-12-01 15:16:22 +0800

    added weather mini project

---
+++ mini-project/weather-tracker/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"encoding/json"
+	"go_playground/mini-project/weather-tracker/api"
+	"net/http"
+	"strings"
+)
+
+func main() {
+	http.HandleFunc("/hello", hello)
+
+	http.HandleFunc("/weather/",
+		func(w http.ResponseWriter, r *http.Request) {
+			city := strings.SplitN(r.URL.Path, "/", 3)[2]
+			data, err := api.Query(city)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			w.Header().Set("Content-Type", "application/json; charset=utf-8")
+			json.NewEncoder(w).Encode(data)
+		})
+
+	http.ListenAndServe(":8080", nil)
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("hello from go!\n"))
+}
