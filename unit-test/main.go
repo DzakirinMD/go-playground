208f3506f1c9df241a7e0350c00a565351922044
DzakirinMD <dzakirinmd@gmail.com>
2023-01-05 16:04:55 +0800

    Apply full unit test

--- unit-test/main.go
+++ unit-test/main.go
@@ -1,9 +1,5 @@
 package main
 
-func main() {
-
-}
-
 // Add @return sum of 2 number
 func Add(x, y int) (res int) {
 	return x + y
@@ -12,4 +8,12 @@ func Add(x, y int) (res int) {
 // Subtract @return difference of 2 number
 func Subtract(x, y int) (res int) {
 	return x - y
+}
+
+// IntMin @ return a if a less than b
+func IntMin(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
 }
--- unit-test/main_test.go
+++ unit-test/main_test.go
@@ -1,7 +1,11 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
+// run go test -v
 func TestAdd(t *testing.T) {
 	got := Add(10, 10)
 	want := 20
@@ -17,5 +21,70 @@ func TestSubtract(t *testing.T) {
 
 	if got != want {
 		t.Errorf("got %q, wanted %q", got, want)
+	}
+}
+
+func TestSubtractTableDriven(t *testing.T) {
+	var tests = []struct {
+		a, b int
+		want int
+	}{
+		{10, 1, 9},
+		{1, 0, 1},
+		{3, 2, 1},
+	}
+
+	for _, tt := range tests {
+		testname := fmt.Sprintf("%d,%d", tt.a, tt.b)
+		// t.Run enables running “subtests”, one for each table entry. These are shown separately when executing go test -v.
+		t.Run(testname, func(t *testing.T) {
+			ans := Subtract(tt.a, tt.b)
+			if ans != tt.want {
+				t.Errorf("got %d, want %d", ans, tt.want)
+			}
+		})
+	}
+}
+
+// Basic test
+func TestIntMinBasic(t *testing.T) {
+	got := IntMin(2, -2)
+	want := -2
+
+	if got != want {
+		t.Errorf("got %q, wanted %q", got, want)
+	}
+}
+
+// Writing tests can be repetitive, so it’s idiomatic to use a table-driven style,
+// where test inputs and expected outputs are listed in a table and a single loop walks over them and performs the test logic.
+func TestIntMinTableDriven(t *testing.T) {
+	var tests = []struct {
+		a, b int
+		want int
+	}{
+		{0, 1, 0},
+		{1, 0, 0},
+		{2, -2, -2},
+		{0, -1, -1},
+		{-1, 0, -1},
+	}
+
+	for _, tt := range tests {
+		testname := fmt.Sprintf("%d,%d", tt.a, tt.b)
+		// t.Run enables running “subtests”, one for each table entry. These are shown separately when executing go test -v.
+		t.Run(testname, func(t *testing.T) {
+			ans := IntMin(tt.a, tt.b)
+			if ans != tt.want {
+				t.Errorf("got %d, want %d", ans, tt.want)
+			}
+		})
+	}
+}
+
+// Benchmarking test
+func BenchmarkIntMin(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		IntMin(1, 2)
 	}
 }
